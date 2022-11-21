33302984033b1ffd4382c0e12db8642ffe761869
Eric Viana <ericvsdefault@gmail.com>
2022-11-21 00:11:48 -0300

    Hide Api Key 🫡

--- Authentication/Auth.js
+++ Authentication/Auth.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAfwvFyZ9K0BBRd97_Q2GxZtbQCLnTbwUs",
+  apiKey: "API_KEY",
   authDomain: "coolab-55229.firebaseapp.com",
   projectId: "coolab-55229",
   storageBucket: "coolab-55229.appspot.com",
